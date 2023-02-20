af58e52ab7028e38b8027cb27988454822ecba6c
Shalitha Viraj <86163983+ShalithaViraj@users.noreply.github.com>
2023-02-20 10:34:15 +0530

    Error correction

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,54 +1,42 @@
-import { AuthenticatedResponse } from './../_interface/AuthenticatedResponse.model';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
-
-@Injectable({
-  providedIn: 'root'
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginModel } from '../_interface/login.model';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { AuthenticatedResponse } from '../_interface/AuthenticatedResponse.model';
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class AuthGuard implements CanActivate  {
+export class LoginComponent implements OnInit {
 
-  constructor(private router:Router, private jwtHelper: JwtHelperService, private http: HttpClient){}
-  
-  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const token = localStorage.getItem("jwt");
+  invalidLogin: boolean;
+  credentials: LoginModel = {UserName:'', Password:''}
 
-    if (token && !this.jwtHelper.isTokenExpired(token)){
-      console.log(this.jwtHelper.decodeToken(token))
-      return true;
-    }
+  constructor(private router: Router, private http: HttpClient){ }
 
-    const isRefreshSuccess = await this.tryRefreshingTokens(token); 
-    if (!isRefreshSuccess) { 
-      this.router.navigate(["login"]); 
-    }
+  ngOnInit(): void {
 
-    return isRefreshSuccess;
   }
 
-  private async tryRefreshingTokens(token: string): Promise<boolean> {
-    const refreshToken: string = localStorage.getItem("refreshToken");
-    if (!token || !refreshToken) { 
-      return false;
+  login = (form: NgForm) =>{
+    if(form.valid){
+      this.http.post<AuthenticatedResponse>("https://localhost:5001/api/auth/login", this.credentials,{
+        headers: new HttpHeaders({"Content-Type":"application/json"})
+      })
+      .subscribe({
+        next: (response:AuthenticatedResponse)=>{
+          const token = response.token;
+          const refreshToken = response.refreshToken;
+        localStorage.setItem("jwt", token); 
+        localStorage.setItem("refreshToken", refreshToken);
+          this.invalidLogin=false;
+          this.router.navigate(["/"]);
+        },
+        error: (err: HttpErrorResponse)=> this.invalidLogin=true
+      })
     }
-    
-    const credentials = JSON.stringify({ accessToken: token, refreshToken: refreshToken });
-    let isRefreshSuccess: boolean;
-    const refreshRes = await new Promise<AuthenticatedResponse>((resolve, reject) => {
-      this.http.post<AuthenticatedResponse>("https://localhost:5001/api/token/refresh", credentials, {
-        headers: new HttpHeaders({
-          "Content-Type": "application/json"
-        })
-      }).subscribe({
-        next: (res: AuthenticatedResponse) => resolve(res),
-        error: (_) => { reject; isRefreshSuccess = false;}
-      });
-    });
-    localStorage.setItem("jwt", refreshRes.token);
-    localStorage.setItem("refreshToken", refreshRes.refreshToken);
-    isRefreshSuccess = true;
-    return isRefreshSuccess;
   }
 
 }
